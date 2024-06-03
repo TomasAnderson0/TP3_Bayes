5ab060b5ef49df56460a13237cdb30674b6a2836
AlejoVaschetti <alejovaschetti@hotmail.com>
2024-06-03 20:21:57 -0300

    akjshdfkjanflj

--- codigo.R
+++ codigo.R
@@ -5,12 +5,54 @@ set.seed(1997)
 beta0 = rnorm(1000, mean = 2.69, sd = 0.017)
 beta1 = rnorm(1000, mean = -0.2465, sd = 0.015)
 varia = abs(rnorm(1000, mean = 0, sd = 0.00749))
-x = -rgamma(1000, shape = 7, scale = 0.08) + 5.883
+x = -rgamma(1000, shape = 5, scale = 0.01) + 5.883
 
 prior = data.frame(beta0, beta1, x)
 
-prior$beta0[1] + prior$beta1[1] * (seq(-1, 24, length.out = 100) - x[1])
-cbind(prior$beta0[1] + prior$beta1[1] * (seq(-1, 24, length.out = 100) - x[1]), 1)
+grid <- seq(4, 14, length.out = 100)
+mu_prior_matrix <- matrix(nrow = 1000, ncol = 100)
+
+for (i in seq_along(grid)) {
+  mu_prior_matrix[, i] <- exp(prior$beta0 + prior$beta1 * (grid[i] - prior$x)) + 22
+}
+
+mu_mean <- apply(mu_matrix, 2, mean)
+mu_qts <- t(apply(mu_matrix, 2, function(x) quantile(x, c(0.025, 0.975))))
+mu_qts2 <- t(apply(mu_matrix, 2, function(x) quantile(x, c(0.25, 0.75))))
+
+# Finalmente, se lamacenan los valores calculados en un data frame
+data_mu_prior <- data.frame(
+  x = x_grid, 
+  y = mu_mean,
+  lower_95 = mu_qts[, 1],
+  upper_95 = mu_qts[, 2],
+  lower_50 = mu_qts2[, 1],
+  upper_50 = mu_qts2[, 2]
+)
+
+ggplot(data_mu_prior) +
+  geom_ribbon(
+    aes(x, ymin = lower_95, ymax = upper_95),
+    fill = "grey50",
+    alpha = 0.6
+  ) +
+  geom_ribbon(
+    aes(x, ymin = lower_50, ymax = upper_50),
+    fill = "grey35",
+    alpha = 0.6
+  ) +
+  geom_line(
+    aes(x, y), 
+    color = "red"
+  ) +
+  geom_hline(yintercept = c(37.5, 36), color = "green") +
+  geom_vline(xintercept = c(5.83), color = "blue") + theme_bw() +
+  scale_x_continuous(breaks = c(4, 5.83, 8, 10, 12, 14, 16, 18, 20, 22, 24),
+                     labels = c("4:00", "5:53", "8:00", "10:00",
+                                "12:00", "14:00", "16:00", "18:00",
+                                "20:00", "22:00", "00:00"),
+                     name = "Hora") +
+  scale_y_continuous(name = "Temperatura (°C)", breaks = c(37.5, 36, 35, 32.5, 30, 27.5, 25, 22))
 
 grafico <- NA
 for(i in  1:200) {
@@ -42,7 +84,13 @@ dgamma(seq(0,5, length.out = 100), shape = 2, scale = 0.5)
 ggplot() + aes(y = dgamma(seq(0,5, length.out = 100), shape = 2, scale = 0.4), x = (-(seq(0,5, length.out = 100)) + 5.883)) +
   geom_line() + geom_vline(xintercept = 5.5)
 
+# Correr el modelo Stan y ver los posterior
 
+init_list <- list(
+  list(b0 = 2.7, b1 = 0.25, sigma = , x =),
+  list(a = 0.5, b = -0.1),
+  list(a = 0.2, b = 0.05)
+)
 
 primera_obs <- list(
   N = 1, 
@@ -58,6 +106,7 @@ modelo1obs <- stan(
   refresh = 0,
   seed = 1997,
   iter = 10000,
+  control = list(adapt_delta = 0.99),
   warmup = 1000
 )
 
@@ -86,6 +135,7 @@ modelo2obs <- stan(
   refresh = 0,
   seed = 1997,
   warmup = 1000,
+  control = list(adapt_delta = 0.99),
   iter = 10000
 )
 
@@ -113,6 +163,7 @@ modelo3obs <- stan(
   refresh = 0,
   seed = 1997,
   warmup = 1000,
+  control = list(adapt_delta = 0.99),
   iter = 10000
 )
 
@@ -138,8 +189,9 @@ ggplot(posterior123) + aes(x = x, fill = observaciones) + geom_density(alpha = 0
 
 grafico_post <- NA
 for(i in  1:1000) {
-  y <-  posterior2$b0[i] + posterior2$b1[i] * (seq(3.5, 24, length.out = 100) - posterior2$x[i])
-  x1 <- seq(3.5, 24, length.out = 100)
+  y <-  posterior2$b0[i] + posterior2$b1[i] * (seq(3.5, 14, length.out = 100) - 
+                                                 posterior2$x[i])
+  x1 <- seq(3.5, 14, length.out = 100)
   muestra <- i
   lol <- cbind(y, x1, muestra)
   grafico_post <- rbind(grafico_post, lol)
@@ -147,10 +199,54 @@ for(i in  1:1000) {
 
 predicciones <- data.frame(y = exp(extract(modelo2obs, "log_dif_temp")$log_dif_temp[,1]) + 22, x = 6.75)
 predicciones <- rbind(data.frame(y = exp(extract(modelo2obs, "log_dif_temp")$log_dif_temp[,2]) + 22, x = 8.25), predicciones)
-View(predicciones)
 
-ggplot() + geom_line(data = grafico_post, aes(x = x1, y = exp(y) + 22, group = muestra),
-                     alpha = 0.01) + 
-  geom_point(aes(x = c(6.75, 8.25), y = c(32.8, 30.5)), shape = 13, size = 3) + 
-  geom_point(data = predicciones, aes(x = x, y = y), alpha = 0.005)
+x_grid <- seq(4, 14, length.out = 100)
+mu_matrix <- matrix(nrow = 27000, ncol = 100)
+
+for (i in seq_along(x_grid)) {
+  mu_matrix[, i] <- exp(posterior2$b0 + posterior2$b1 * (x_grid[i] - posterior2$x)) + 22
+}
+
+mu_mean <- apply(mu_matrix, 2, mean)
+mu_qts <- t(apply(mu_matrix, 2, function(x) quantile(x, c(0.025, 0.975))))
+mu_qts2 <- t(apply(mu_matrix, 2, function(x) quantile(x, c(0.25, 0.75))))
+
+data_mu <- data.frame(
+  x = x_grid, 
+  y = mu_mean,
+  lower_95 = mu_qts[, 1],
+  upper_95 = mu_qts[, 2],
+  lower_50 = mu_qts2[, 1],
+  upper_50 = mu_qts2[, 2]
+)
+
+ggplot() + 
+  geom_ribbon(
+    aes(x, ymin = lower_95, ymax = upper_95),
+    fill = "grey50",
+    alpha = 0.6,
+    data = data_mu
+  ) +
+  geom_ribbon(
+    aes(x, ymin = lower_50, ymax = upper_50),
+    fill = "grey35",
+    alpha = 0.6,
+    data = data_mu
+  ) +
+  geom_line(
+    aes(x, y), 
+    color = "firebrick",
+    data = data_mu
+  ) +
+  stat_summary(data = predicciones, fun.data = mean_sdl,
+               mapping = aes(x = x, y = y), color = "blue", size = 0.5) +
+  scale_y_continuous(limits = c(28, 36), breaks = seq(28, 36, 1),
+                     name = "Temperatura (ºC)") + 
+  scale_x_continuous(limits = c(6, 9), breaks = seq(6, 9, 0.5), 
+                     labels = c("6:00", "6:30", "7:00", "7:30",
+                                "8:00", "8:30", "9:00"), 
+                     name = "Hora") +
+  geom_point(aes(x = c(6.75, 8.25), y = c(32.8, 30.5)),
+             shape = 13, size = 2, color = "red") +
+  theme_bw()
 
